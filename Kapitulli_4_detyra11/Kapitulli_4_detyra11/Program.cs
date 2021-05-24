706bfeeacddfa2a5e1d1de3547ed96e77bdc375b
Gezim Kastrati <gk48181@universum-ks.org>
2021-05-25 00:55:22 +0200

    My fourth commit

---
+++ Kapitulli_4_detyra11/Kapitulli_4_detyra11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapitulli_4_detyra11
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int sum = 0;
+
+            Console.Write("Enter number: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= length; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
