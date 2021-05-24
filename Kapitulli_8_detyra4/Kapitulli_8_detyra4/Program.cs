561aed70f6695ec064e7475b400f4c28350ebe94
Gezim Kastrati <gk48181@universum-ks.org>
2021-05-25 01:50:17 +0200

    My eighth commit

---
+++ Kapitulli_8_detyra4/Kapitulli_8_detyra4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapitulli_8_detyra4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter decimal number: ");
+            int deci = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("{0} to binary is {1}.", deci, Convert.ToString(deci, 2));
+        }
+    }
+}
