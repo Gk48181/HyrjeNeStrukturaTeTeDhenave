230e32365cb22256297ad17102b5372a3170c021
Gezim Kastrati <gk48181@universum-ks.org>
2021-05-25 01:57:01 +0200

    My ninth commit

---
+++ Kapitulli_9_detyra1/Kapitulli_9_detyra1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapitulli_9_detyra1
+{
+    class Program
+    {
+
+        static void ReturnName(string name)
+        {
+            Console.WriteLine("Hello {0}", name);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter name: ");
+            ReturnName(Console.ReadLine());
+        }
+    }
+}
